a0168a318912c19f77b94ba4eb42371bcde77c0c
KyleTheHack3r <thepmteam@hotmail.com>
2015-07-22 19:43:43 +0100

    Update Give.java

--- command/vanilla/Give.java
+++ command/vanilla/Give.java
@@ -8,14 +8,21 @@ public class Give implements CommandExecutor {
     public String getCommand() {
         return "give";
     }
+    public final String getName() {
+        return this.username;
+    }
     @Override
     public boolean onCommand(CommandSender sender, String label, String[] args) {
         if(args.length < 2) {
             return "commands.give.usage";
         } else if(args.length == 2){
-            String func = args[0];
+            String p = args[0];
             String id = args[1];
+            String amount = args[2];
             String result = null;
+            if(getOnlinePlayers() != null && result != null){
+
+            }
         }
 
 
